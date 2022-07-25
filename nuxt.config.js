691908f59bcfee17e2469cfc40daf2437acb0866
khachoangpt <hoangncd3@gmail.com>
2022-07-25 17:45:51 +0700

    fix call api deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL:
-      'http://humanresourcemanagementsystem-env-2.eba-mutdm3hm.us-west-2.elasticbeanstalk.com/hrm/',
+      'http://humanresourcemanagementsystem-env-2.eba-mutdm3hm.us-west-2.elasticbeanstalk.com/hrm',
     proxyHeaders: false,
     credentials: false,
   },
