7ec1140314acd143bfef9dfd32e8161c60960017
khachoangpt <hoangncd3@gmail.com>
2022-07-28 14:28:30 +0700

    fix base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL:
-      'https://www.hronline.tk/',
+      'https://www.hronline.tk/hrm',
     proxyHeaders: false,
     credentials: false,
   },
